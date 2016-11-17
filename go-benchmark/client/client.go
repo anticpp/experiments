9a6b649a52e3aed38b6631d7029fee3be92455e7
supergui <supergui@live.cn>
2016-11-17 10:21:05 +0800

    Add client stat info

--- go-benchmark/client/client.go
+++ go-benchmark/client/client.go
@@ -1,15 +1,49 @@
 package main
 
 import (
-	"fmt"
 	"flag"
-	"net"
-	"time"
+	"fmt"
 	"github.com/anticpp/experiments/go-benchmark/pack"
+	"net"
+	"sync"
+	"sync/atomic"
+	"time"
 )
+
+type statInfo struct {
+	completeRequests int32
+	failedRequests   int32
+
+	mutexAvgRequestMs  sync.Mutex
+	avgRequestMs       float64
+	avgRequestNSamples int32
+}
+
+func (s *statInfo) incCompleteRequests() {
+	atomic.AddInt32(&s.completeRequests, 1)
+}
+func (s *statInfo) incFailedRequests() {
+	atomic.AddInt32(&s.failedRequests, 1)
+}
+func (s *statInfo) calAvgRequestMs(ms float64) {
+	s.mutexAvgRequestMs.Lock()
+	defer s.mutexAvgRequestMs.Unlock()
+
+	fmt.Printf("calAvgRequestMs %0.2f\n", ms)
+	s.avgRequestNSamples++
+
+	n := float64(s.avgRequestNSamples)
+	s.avgRequestMs = ((n-1)/n)*s.avgRequestMs + ms/n
+}
+
+var stat = &statInfo{completeRequests: 0,
+	failedRequests:     0,
+	avgRequestMs:       0.0,
+	avgRequestNSamples: 0}
+
 var (
-	packSize int
-	readTimeout int
+	packSize     int
+	readTimeout  int
 	writeTimeout int
 )
 
@@ -23,7 +57,7 @@ func makeRequest(conn net.Conn) error {
 	req := pack.New(packSize)
 	conn.SetWriteDeadline(time.Now().Add(time.Duration(writeTimeout) * time.Second))
 	_, err = conn.Write(req.Raw())
-	if err!=nil {
+	if err != nil {
 		return err
 	}
 
@@ -34,7 +68,7 @@ func makeRequest(conn net.Conn) error {
 	for pos < len(rbuf) {
 		conn.SetReadDeadline(time.Now().Add(time.Duration(readTimeout) * time.Second))
 		n, err = conn.Read(rbuf[pos:])
-		if err!=nil {
+		if err != nil {
 			fmt.Println(err)
 			return err
 		}
@@ -53,32 +87,40 @@ func worker(name string, addr string, requests int, c chan int) {
 	var err error
 
 	total := 0
-	for retry:=0; retry<3; retry++ {
+	for retry := 0; retry < 3; retry++ {
 
-		if retry>0 {
+		if retry > 0 {
 			fmt.Printf("[%v] Wait interval to retry, %v\n", name, retry)
 			time.Sleep(3 * time.Second)
 		}
 
 		conn, err = net.Dial("tcp", addr)
-		if err!=nil {
+		if err != nil {
 			fmt.Println(name, err)
 			continue
 		}
 
-		for ; total<requests; total++ {
+		for ; total < requests; total++ {
 
-			//fmt.Printf("[%v] %v request\n", name, total)
+			startMs := float64((time.Now().UnixNano()) / 1000000)
+
 			err = makeRequest(conn)
-			if err!=nil {
+			if err != nil {
+				stat.incFailedRequests()
 				break
 			}
+
+			endMs := float64((time.Now().UnixNano()) / 1000000)
+			totalMs := endMs - startMs
+
+			stat.incCompleteRequests()
+			stat.calAvgRequestMs(totalMs)
 		}
 
 		conn.Close()
 
-		// Complete all requests, otherwise some error happened. 
-		if total>=requests {
+		// Complete all requests, otherwise some error happened.
+		if total >= requests {
 			break
 		}
 		// else continue
@@ -113,17 +155,41 @@ func main() {
 	var addr = fmt.Sprintf("%v:%v", ip, port)
 	c := make(chan int, concurency)
 
-	fmt.Printf("Addr %v, concurency %v, requests %v\n", addr, concurency, nRequest)
-	for i:=0; i<concurency; i++ {
+	fmt.Printf("Server Addr %v\n", addr)
+	fmt.Printf("Concurency %v\n", concurency)
+	fmt.Printf("Total Requests %v(%v*%v)\n", concurency*nRequest, concurency, nRequest)
+	fmt.Println("")
+	fmt.Println("Benchmarking ... wait")
+
+	startMs := float64((time.Now().UnixNano()) / 1000000)
+	for i := 0; i < concurency; i++ {
 		name := fmt.Sprintf("worker_%v", i)
 		go worker(name, addr, nRequest, c)
 	}
-	fmt.Printf("%v worker spawned\n", concurency)
 
 	// Wait worker complete
-	for i:=0; i<concurency; i++ {
+	for i := 0; i < concurency; i++ {
 		<-c
 	}
 
-	fmt.Println("Finished")
+	endMs := float64((time.Now().UnixNano()) / 1000000)
+	totalMs := endMs - startMs
+
+	if totalMs == 0 {
+		totalMs = 1
+	}
+
+	qps := float64(stat.completeRequests) / totalMs
+	tpr := float64(0.00)
+	if stat.completeRequests > 0 {
+		tpr = totalMs / float64(stat.completeRequests)
+	}
+
+	fmt.Printf("\n\n")
+	fmt.Printf("Complete Requests:           %v\n", stat.completeRequests)
+	fmt.Printf("Failed Requests:             %v\n", stat.failedRequests)
+	fmt.Printf("Total Time:                  %0.2f [sec]\n", totalMs/1000)
+	fmt.Printf("Requests per second:         %0.2f\n", qps)
+	fmt.Printf("Time per request:            %0.2f [ms] (Across all concurency)\n", tpr)
+	fmt.Printf("Time per request:            %0.2f [ms] (Average)\n", stat.avgRequestMs)
 }
