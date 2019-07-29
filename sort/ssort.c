dbca4f1aa3d75f76ab73d37c4133f173235be636
supergui <supergui@live.cn>
2019-07-29 10:35:22 +0800

    Improve ssort

--- sort/ssort.c
+++ sort/ssort.c
@@ -2,11 +2,18 @@
  */
 void ssort(int arr[], int start, int end) {
     for(int i=start; i<end; i++) {
+        // Find `min`
+        int min = i;
         for(int j=i+1; j<=end; j++) {
             cnt_inc_comp();
-            if(arr[i]>arr[j]) {
-                swap(arr, i, j);
+            if(arr[j]<arr[min]) {
+                min = j;
             }
+        }
+
+        // Swap
+        if(min!=i) {
+            swap(arr, i, min);
         }
     }
 }
